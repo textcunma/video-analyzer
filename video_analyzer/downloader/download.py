b41a92192b06c5fc4589ccd12a333297e7d7a0f8
Your Name <you@example.com>
2023-06-03 13:57:09 +0900

    add download codes and update env.yml about download

--- video_analyzer/downloader/download.py
+++ video_analyzer/downloader/download.py
@@ -1,25 +1,26 @@
 import os
-from typing import Any
 
+from yt_dlp import YoutubeDL
 
-class DownloadVideo:
+class VideoDownloader:
     """1つの映像ファイルをダウンロードするクラス
     """
-    def __init__(self, url, save_dir) -> None:
-        """
+    def __init__(self, url: str, save_dir: str) -> None:
+        """イニシャライザ（インスタンスの初期化）
         :param args 引数
         """
         # 保存ディレクトリ作成
         os.makedirs(save_dir, exist_ok=True)
 
-        # プライベートメンバ変数
-        self.__save_dir = save_dir
-        self.__download_url = args.download_url
+        # プライベートインスタンス変数(見かけだけプライベート)
+        self._save_dir = save_dir
+        self._download_url = url
+        self._video_name = url.split('/')[-1]
 
-
-    def __call__(self) -> Any:
-        pass
-
+    def __call__(self) -> None:
+        ydl_opts = {'format': 'best', 'outtmpl' : self._save_dir+self._video_name+'_.mp4'}
+        with YoutubeDL(ydl_opts) as ydl:
+            ydl.download([self._download_url])
 
 # テスト用コード
 if __name__ == "__main__":
@@ -27,7 +28,12 @@ if __name__ == "__main__":
 
     parser = argparse.ArgumentParser(description="コマンドライン引数")
     parser.add_argument(
-        "--download_dir", type=str, default="./movie/", help="ダウンロードしたファイルを置くディレクトリ"
+        "--save_dir", type=str, default="./movie/", help="ダウンロードしたファイルを保存するディレクトリ"
     )
-    parser.add_argument("--download_url", default=['https://youtu.be/R9JBN_x9zFQ','https://youtu.be/lgKjhlmTqek'], help="ダウンロードしたい動画のURL")
+    parser.add_argument("--download_url", default=['https://youtu.be/lgKjhlmTqek'], help="ダウンロードしたい動画のURL")
     args = parser.parse_args()
+
+    # ダウンロード
+    [VideoDownloader(url, args.save_dir)() for url in args.download_url]
+
+
